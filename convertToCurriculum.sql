85ebea3c8a4a305bea6f9c828b4e6d486e626b71
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2020-11-13 10:34:53 -0500

    Fixed whitespace.

--- convertToCurriculum.sql
+++ convertToCurriculum.sql
@@ -12,8 +12,8 @@ DECLARE
     resultant_curriculum_unit_content_id INT;
     resultant_curriculum_topic_content_id INT;
     resultant_curriculum_topic_question_id INT;
-	course_topic_assessment_row topic_assessment_info%ROWTYPE;
-	course_question_assessment_row course_question_assessment_info%ROWTYPE;
+    course_topic_assessment_row topic_assessment_info%ROWTYPE;
+    course_question_assessment_row course_question_assessment_info%ROWTYPE;
 BEGIN
     -- Create a Curriculum object from the base Course object
     SELECT * INTO course_row FROM course WHERE course_active = true AND course_id = $1;
@@ -34,7 +34,7 @@ BEGIN
     (
          DEFAULT, -- curriculum id
          course_row.university_id, -- university id
-		 -- Uncomment this for a cheap hack to let you keep testing this script on the same course.
+         -- Uncomment this for a cheap hack to let you keep testing this script on the same course.
          -- course_row.course_name || (random() * 100000)::"text",
          course_row.course_name,
          course_row.course_textbooks,
@@ -96,43 +96,43 @@ BEGIN
                  NOW()
              )
              RETURNING curriculum_topic_content_id INTO resultant_curriculum_topic_content_id;
-			 
+
                 --  Select the assessment info that belongs to this topic. There should only be one.
-    			SELECT * INTO course_topic_assessment_row FROM topic_assessment_info 
-				WHERE topic_assessment_info_active = true AND topic_assessment_info.course_topic_content_id = topic_record.course_topic_content_id;
-	
+                SELECT * INTO course_topic_assessment_row FROM topic_assessment_info
+                WHERE topic_assessment_info_active = true AND topic_assessment_info.course_topic_content_id = topic_record.course_topic_content_id;
+
                 -- Copy over the fields to the curriculum version of the assessment info.
-				IF course_topic_assessment_row.topic_assessment_info_id NOTNULL THEN
-					INSERT INTO curriculum_topic_assessment_info (
-						curriculum_topic_assessment_info_id,
-						curriculum_topic_content_id,
-						curriculum_topic_assessment_info_duration,
-						curriculum_topic_assessment_info_hard_cutoff,
-						curriculum_topic_assessment_info_max_graded_attempts_per_version,
-						curriculum_topic_assessment_info_max_versions,
-						curriculum_topic_assessment_info_version_delay,
-						curriculum_topic_assessment_info_hide_hints,
-						curriculum_topic_assessment_info_show_itemized_results,
-						curriculum_topic_assessment_info_show_total_grade_immediately,
-						curriculum_topic_assessment_info_hide_problems_after_finish,
-						curriculum_topic_assessment_info_randomize_order,
-						curriculum_topic_assessment_info_active
-					) VALUES (
-						DEFAULT,
-						resultant_curriculum_topic_content_id,
-						course_topic_assessment_row.topic_assessment_info_duration,
-						course_topic_assessment_row.topic_assessment_info_hard_cutoff,
-						course_topic_assessment_row.topic_assessment_info_max_graded_attempts_per_version,
-						course_topic_assessment_row.topic_assessment_info_max_versions,
-						course_topic_assessment_row.topic_assessment_info_version_delay,
-						course_topic_assessment_row.topic_assessment_info_hide_hints,
-						course_topic_assessment_row.topic_assessment_info_show_itemized_results,
-						course_topic_assessment_row.topic_assessment_info_show_total_grade_immediately,
-						course_topic_assessment_row.topic_assessment_info_hide_problems_after_finish,
-						course_topic_assessment_row.topic_assessment_info_randomize_order,
-						course_topic_assessment_row.topic_assessment_info_active
-					);
-				END IF;
+                IF course_topic_assessment_row.topic_assessment_info_id NOTNULL THEN
+                    INSERT INTO curriculum_topic_assessment_info (
+                        curriculum_topic_assessment_info_id,
+                        curriculum_topic_content_id,
+                        curriculum_topic_assessment_info_duration,
+                        curriculum_topic_assessment_info_hard_cutoff,
+                        curriculum_topic_assessment_info_max_graded_attempts_per_version,
+                        curriculum_topic_assessment_info_max_versions,
+                        curriculum_topic_assessment_info_version_delay,
+                        curriculum_topic_assessment_info_hide_hints,
+                        curriculum_topic_assessment_info_show_itemized_results,
+                        curriculum_topic_assessment_info_show_total_grade_immediately,
+                        curriculum_topic_assessment_info_hide_problems_after_finish,
+                        curriculum_topic_assessment_info_randomize_order,
+                        curriculum_topic_assessment_info_active
+                    ) VALUES (
+                        DEFAULT,
+                        resultant_curriculum_topic_content_id,
+                        course_topic_assessment_row.topic_assessment_info_duration,
+                        course_topic_assessment_row.topic_assessment_info_hard_cutoff,
+                        course_topic_assessment_row.topic_assessment_info_max_graded_attempts_per_version,
+                        course_topic_assessment_row.topic_assessment_info_max_versions,
+                        course_topic_assessment_row.topic_assessment_info_version_delay,
+                        course_topic_assessment_row.topic_assessment_info_hide_hints,
+                        course_topic_assessment_row.topic_assessment_info_show_itemized_results,
+                        course_topic_assessment_row.topic_assessment_info_show_total_grade_immediately,
+                        course_topic_assessment_row.topic_assessment_info_hide_problems_after_finish,
+                        course_topic_assessment_row.topic_assessment_info_randomize_order,
+                        course_topic_assessment_row.topic_assessment_info_active
+                    );
+                END IF;
 
               FOR problem_record IN execute 'SELECT * FROM course_topic_question WHERE course_topic_question_active = true AND course_topic_content_id = ' || topic_record.course_topic_content_id
               LOOP
@@ -164,27 +164,26 @@ BEGIN
                   )
                   RETURNING curriculum_topic_question_id INTO resultant_curriculum_topic_question_id;
 
-                  			 
                 --  Select the assessment info that belongs to this question. There should only be one.
-    			SELECT * INTO course_question_assessment_row FROM course_question_assessment_info 
-				WHERE course_question_assessment_info_active = true AND course_question_assessment_info.course_topic_question_id = problem_record.course_topic_question_id;
-	
+                SELECT * INTO course_question_assessment_row FROM course_question_assessment_info
+                WHERE course_question_assessment_info_active = true AND course_question_assessment_info.course_topic_question_id = problem_record.course_topic_question_id;
+
                 -- Copy over the fields to the curriculum version of the assessment info.
-				IF course_question_assessment_row.course_question_assessment_info_id NOTNULL THEN
-					INSERT INTO curriculum_question_assessment_info (
+                IF course_question_assessment_row.course_question_assessment_info_id NOTNULL THEN
+                    INSERT INTO curriculum_question_assessment_info (
                         curriculum_question_assessment_info_id,
                         curriculum_topic_question_id,
                         curriculum_question_assessment_info_random_seed_set,
                         curriculum_question_assessment_info_additional_problem_paths,
                         curriculum_question_assessment_info_active
-					) VALUES (
-						DEFAULT,
-						resultant_curriculum_topic_question_id,
-						course_question_assessment_row.course_question_assessment_info_random_seed_set,
-						course_question_assessment_row.course_question_assessment_info_additional_problem_paths,
-						course_question_assessment_row.course_question_assessment_info_active
-					);
-				END IF;
+                    ) VALUES (
+                        DEFAULT,
+                        resultant_curriculum_topic_question_id,
+                        course_question_assessment_row.course_question_assessment_info_random_seed_set,
+                        course_question_assessment_row.course_question_assessment_info_additional_problem_paths,
+                        course_question_assessment_row.course_question_assessment_info_active
+                    );
+                END IF;
               END LOOP;
          END LOOP;
     END LOOP;
